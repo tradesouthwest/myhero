4c158dcbf6d1a26042cb6480a74bc1a1a4747955
Larry <tradesouthwest@gmail.com>
2026-08-31 15:42:57 -0700

    safecss_filter_attr

--- front-page.php
+++ front-page.php
@@ -19,7 +19,8 @@ get_header();
 
 <main id="primary" class="site-main">
 
-	<section class="hero-atf <?php echo esc_attr( 'has-media-' . $hero['bg_type'] ); ?>" <?php echo $hero_style; ?>>
+	<section class="hero-atf <?php echo esc_attr( 'has-media-' 
+		. $hero['bg_type'] ); ?>" <?php echo safecss_filter_attr( $hero_style ); ?>>
 	    
 		<?php if ( 'video' === $hero['bg_type'] && ! empty( $hero['bg_video'] ) ) : ?>
 			<div class="hero-video-wrapper">
